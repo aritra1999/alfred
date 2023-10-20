8b83d1bcf4e46f006517e4e3bebdd9b489b79b0f
Aritra Mondal <aritra041999.dev@gmail.com>
2023-10-20 12:48:47 +0530

    Add database connection utility

---
+++ models/setup.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func ConnectDataBase() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	Dbdriver := os.Getenv("DB_DRIVER")
+	DbHost := os.Getenv("DB_HOST")
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbPort := os.Getenv("DB_PORT")
+
+	DSN := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", DbHost, DbUser, DbPassword, DbName, DbPort)
+
+	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Cannot connect to database ", Dbdriver)
+		log.Fatal("connection error:", err)
+	} else {
+		fmt.Println("We are connected to the database ", Dbdriver)
+	}
+
+	DB.AutoMigrate(&User{})
+
+}
