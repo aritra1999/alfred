3878c9849228ef9ff517500ad7357daf9ef81e4d
Aritra Mondal <aritra041999.dev@gmail.com>
2023-11-04 12:33:09 +0530

    Update authentication controllers

--- controllers/auth.go
+++ controllers/auth.go
@@ -2,29 +2,26 @@ package controllers
 
 import (
 	"albert/models"
+	"albert/services/auth"
+	"albert/services/cache"
 	"albert/services/mail"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type SignUpInput struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email string `json:"email" binding:"required"`
 }
 
-type SignInInput struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+type MagicLinkInput struct {
+	Email string `json:"email" binding:"required"`
 }
 
-// SignUp      	 godoc
-// @Summary      SignUp endpoint for creating new user
-// @Description  Creates new user
-// @Tags         auth
-// @Produce      json
-// @Param        user  body  SignUpInput  true  "User details"
-// @Router       /auth/signup [post]
 func SignUp(c *gin.Context) {
 	var input SignUpInput
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -34,7 +31,6 @@ func SignUp(c *gin.Context) {
 
 	u := models.User{}
 	u.Email = input.Email
-	u.Password = input.Password
 
 	if _, err := u.SaveUser(); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
@@ -46,23 +42,55 @@ func SignUp(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "user created successfully"})
 }
 
-func SignIn(c *gin.Context) {
-	var input SignInInput
+func SendMagicLink(c *gin.Context) {
+	user := models.User{}
+
+	var input MagicLinkInput
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	u := models.User{}
-	u.Email = input.Email
-	u.Password = input.Password
-
-	token, err := models.SingInCheck(u.Email, u.Password)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid credentials"})
+	user.Email = input.Email
+	if err := models.CheckUser(user.Email); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"token": token})
+	magicLinkToken := auth.GenerateMagicLinkToken()
+	cache.Set(magicLinkToken, user.Email, 10*time.Minute)
+
+	magicLink := fmt.Sprintf("http://%s/auth/magic-link?token=%s", os.Getenv("FRONTEND_URL"), magicLinkToken)
+	if _, err := mail.SendEmail(user.Email, "CareerLab OTP", fmt.Sprintf("Your magic link is %s (valid for 10 mins)", magicLink)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error sending magic link, please try again later"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Magic link sent to %s, please check email", user.Email)})
+}
+
+func ValidateMagicLink(c *gin.Context) {
+	token := c.Query("token")
+
+	log.Println(token)
+
+	email, err := cache.Get(token)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid token"})
+		return
+	}
+
+	user, err := models.GetUserByEmail(email)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	jwtToken, err := user.GenerateToken()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating token!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": jwtToken})
 }
